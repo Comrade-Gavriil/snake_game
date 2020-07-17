fb665d2b4436ccd8f0dde3a7fb976df868d74d52
Bodhi-Rubinstein <bodhirubinstein@gmail.com>
2020-07-17 17:20:54 -0600

    Weird

--- ai.py
+++ ai.py
@@ -3,8 +3,4 @@ from client import client
 class ai(client): #inherits client class
 
     def __init__ (self,key,url):
-        super.__init__(key, url)
-
-
-
-    
+        super.__init__(key, url)    
