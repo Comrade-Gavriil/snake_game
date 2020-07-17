34cec34adc968e6f967f316805c4d767edff8197
Graham Dobbie <comradegavriil@gmail.com>
2020-07-17 17:15:15 -0600

    stuff aswell

--- ai.py
+++ ai.py
@@ -1,6 +1,14 @@
-import client
+from client import client
 
 class ai(client): #inherits client class
 
     def __init__ (self,key,url):
         super.__init__(key, url)
+    
+    def _make_path(self):
+        
+        food = self.food
+        board = self.board
+        heads = self.heads
+
+        
