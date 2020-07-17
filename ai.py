aaf69ff24b3905d179000f41e0682bbe16886898
Graham Dobbie <comradegavriil@gmail.com>
2020-07-17 17:57:54 -0600

    _make_path

--- ai.py
+++ ai.py
@@ -5,10 +5,44 @@ class ai(client): #inherits client class
     def __init__ (self,key,url):
         super.__init__(key, url)
     
-    def _make_path(self):
+    @staticmethod
+    def vector_add(v1,v2):
+        return [ (v1[0]+v2[0]), (v1[1] + v2[1])]
+    
+    @staticmethod
+    def vector_sub(v1,v2):
+        return [ (v1[0]-v2[0]), (v1[1] - v2[1])]
+    
+    @staticmethod
+    def vector_mult(v1,v2):
+        return [ (v1[0]*v2[0]), (v1[1] * v2[1])]
+
+    
+
+    def _make_path(self, scr, dst):
         
-        food = self.food
-        board = self.board
-        heads = self.heads
+        rel_pos = self.vector_sub(dst, scr)
+
+        def direction(rel):
+            direction = []
+            for cord in cords:
+                if cord > 1:
+                    direction.append(1)
+                elif cord == 0:
+                    direction.append(0)
+                elif cord == 0:
+                    direction.append(-1)
+        
+        move_rel_cords = direction(rel_pos)
 
         
+
+
+
+
+
+
+
+
+
+
